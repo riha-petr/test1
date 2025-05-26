7a821692a62209108d27778a6c622a755886037f
riha-petr <petr-riha@seznam.cz>
2025-05-26 06:19:00 +0200

    Remove inline footer section

--- musica.php
+++ musica.php
@@ -117,39 +117,6 @@
 			</div>
 		</div>
 	</section>
-
-	<section class="footer">
-		<div class="footer-content">
-			<div class="boxing">
-				<ul>
-					<li>Home</li>
-					<li>Music</li>
-					<li>Poetry</li>
-					<li>Register</li>
-				</ul>
-			</div>
-			<div class="boxing">
-				<ul>
-					<li>Sign in</li>
-					<li>Donation</li>
-				</ul>
-			</div>
-			<div class="boxing">
-				<ul>
-					<li>About us</li>
-					<li>Contact us</li>
-					<li>Location</li>
-				</ul>
-			</div>
-			<div class="boxing">
-				<ul>
-					<li><i class="fa-3x fa fa-twitter"></i></li>
-					<li class="fa-3x fa fa-youtube"></li>
-					<li><i class="fa-3x fa fa-bandcamp"></i></li>
-				</ul>
-			</div>
-		</div>
-	</section>
 	<script type="text/javascript">
 		function _(query){
 			return document.querySelector(query);
